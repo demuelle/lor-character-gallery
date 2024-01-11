4cbaf9a6494fce64be20c255f1a896b1c6e1168d
demuelle <dmueller@trilogyed.com>
2024-01-11 14:58:40 -0600

    First commit

---
+++ src/components/CharacterGallery.jsx
@@ -0,0 +1,13 @@
+import chars from "../data/characterData.json";
+import Character from "./Character";
+
+const CharacterGallery = () => {
+    return (
+        <>
+        {chars.map(c =>  <Character key={c._id} {...c} />)}
+        </>
+    );
+}
+
+export default CharacterGallery;
+
